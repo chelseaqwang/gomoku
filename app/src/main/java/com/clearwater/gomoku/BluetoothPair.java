3935881b125650ad359d92a1bc20b4f7f37576a1
chenbo <chenbo@svl13.cs.pdx.edu>
2015-02-25 10:33:07 -0800

    bluetooth-debugging

--- app/src/main/java/com/clearwater/gomoku/BluetoothPair.java
+++ app/src/main/java/com/clearwater/gomoku/BluetoothPair.java
@@ -1,9 +1,14 @@
 package com.clearwater.gomoku;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 
 public class BluetoothPair extends ActionBarActivity {
@@ -12,6 +17,38 @@ public class BluetoothPair extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth_pair);
+
+        Button button_host = (Button)findViewById(R.id.button_host);
+        button_host.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        int size = 0;
+                        RadioGroup sizeGroup = (RadioGroup)findViewById(R.id.size);
+                        int sizeSelected = sizeGroup.getCheckedRadioButtonId();
+                        RadioButton sizeRB = (RadioButton)findViewById(sizeSelected);
+                        switch(sizeRB.getText().toString()) {
+                            case "10*10":    size = 10; break;
+                            case "15*15":    size = 15; break;
+                            case "20*20":    size = 20; break;
+                        }
+
+                        Intent mainIntent = new Intent(BluetoothPair.this, BT_host.class);
+                        mainIntent.putExtra("SIZE", size);
+                        BluetoothPair.this.startActivity(mainIntent);
+                    }
+                }
+        );
+
+        Button button_guest = (Button)findViewById(R.id.button_join);
+        button_guest.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent mainIntent = new Intent(BluetoothPair.this, BT_guest.class);
+                        BluetoothPair.this.startActivity(mainIntent);
+                    }
+                }
+        );
+
     }
 
 }
